cfe571b4a04228c5fe999792fc75b38c9bb449a9
“Gary <garynye@gmail.com>
2021-12-06 12:39:55 -0600

    materialui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
+import "./App.css";
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <a
-          className="App-link"
-          href="mailto:gary@garynye.com"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            backgroundImage: `url("https://res.cloudinary.com/assurily/image/upload/c_scale,w_1159/v1638810851/garynyedotcom/IMG_5190_nhde57_dwda6q.webp")`,
+            width: "100%",
+            height: "30%",
+          }}
         >
-          Click to Email
-        </a>
+          <a style={{ textDecoration: "none" }} href="mailto:gary@garynye.com">
+            <Button variant="contained">Email Me</Button>
+          </a>
+        </div>
       </header>
     </div>
   );
